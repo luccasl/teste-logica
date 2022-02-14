9952f35eb45e804b8289e42765f48d13996a4bff
luccasl <luccasleite@hotmail.com>
2022-02-14 19:14:36 -0300

    solucionar teste

--- src/ProductStructure.php
+++ src/ProductStructure.php
@@ -20,7 +20,23 @@ class ProductStructure
 
     public function make(): array
     {
-       //todo your code.
-        return [];
+        $productSizes = array();
+
+        $productCounts = array_count_values(self::products);
+
+        array_walk($productCounts, function($item, $key) use(&$productSizes) {
+            $productDescription = explode('-', $key);
+            $productColor = $productDescription[0];
+            $productSize = $productDescription[1];
+
+            $containsProductColor = array_key_exists($productColor, $productSizes);
+            if(!$containsProductColor) {
+                $productSizes[$productColor] = array();
+            }
+
+            $productSizes[$productColor][$productSize] = $item;
+        });
+
+        return $productSizes;
     }
 }
